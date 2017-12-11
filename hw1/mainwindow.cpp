7fefa30f5e4206a204449d7ae44bf9a5f6a3f027
ysl <yangshuoliu@sina.com>
2017-12-11 11:22:37 +0800

    .

---
+++ hw1/mainwindow.cpp
@@ -0,0 +1,83 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::on_videoOpenBtn_clicked()
+{
+   m_videoName = QFileDialog::getOpenFileName(this,QStringLiteral("Video Selection"),"",tr("Video Files (*.avi *.rmvb)"));
+   if(m_videoName == QString())
+       return;
+   ui->videoListCBox->clear();
+   ui->videoListCBox->addItem(m_videoName);
+}
+
+void MainWindow::on_imagesOpenBtn_clicked()
+{
+     m_imageNames = QFileDialog::getOpenFileNames(this,QStringLiteral("Images Selection"),"",tr("Image Files (*.png *.jpg *.bmp)"));
+     if(m_imageNames.empty() == true)
+         return;
+     ui->imageListCBox->clear();
+     ui->imageListCBox->addItems(m_imageNames);
+}
+
+void MainWindow::on_processBtn_clicked()
+{
+    cv::VideoCapture cap(m_videoName.toStdString());
+    if(cap.isOpened() == false){
+        QMessageBox::critical(this,QStringLiteral("Error"),QStringLiteral("Video file does not exsit\n"),QMessageBox::Ok,QMessageBox::Ok);
+        return;
+    }
+
+    //Get Video Info
+    int frameCount = cap.get(CV_CAP_PROP_FRAME_COUNT);
+    int frameWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+    int frameHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+    double fps = cap.get(CV_CAP_PROP_FPS);
+
+    QString vinfo = tr("\tFrame Count:%1\n").arg(frameCount)
+            +tr("\tFrame Width:%1").arg(frameWidth)
+            +tr("\tFrame Height:%1").arg(frameHeight)
+            +tr("\tFps:%1\n").arg(fps);
+
+    ui->videoInfoTextEdit->clear();
+    ui->videoInfoTextEdit->setText(vinfo);
+
+
+    QVector<cv::Mat> images;
+    foreach(const QString & name,m_imageNames){
+        cv::Mat image = cv::imread(name.toStdString());
+        if(image.empty() == true){
+            QMessageBox::critical(this,QStringLiteral("Error"),tr("%1 can not be read").arg(name),QMessageBox::Ok,QMessageBox::Ok);
+        }
+        images.push_back(image);
+    }
+
+
+
+    //Resize Images
+    QVector<cv::Mat> images;
+
+}
+
+void MainWindow::on_outputPathBtn_clicked()
+{
+    m_outputName = QFileDialog::getSaveFileName(this,QStringLiteral("Save"),"",tr("Video (*.avi *.rmvb)"));
+    ui->outputPathEdit->clear();
+    ui->outputPathEdit->setText(m_outputName);
+}
+
+
+void MainWindow::on_closeBtn_clicked()
+{
+
+}
---
+++ hw1/mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QVector>
+#include <QStringList>
+#include <QString>
+#include <QFileInfo>
+#include <QMessageBox>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+
+    void on_videoOpenBtn_clicked();
+
+    void on_imagesOpenBtn_clicked();
+
+    void on_processBtn_clicked();
+
+    void on_outputPathBtn_clicked();
+
+    void on_closeBtn_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QStringList m_imageNames;
+    QString m_videoName;
+    QString m_outputName;
+};
+
+#endif // MAINWINDOW_H
